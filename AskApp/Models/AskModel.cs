552d15e089b0850030137a6acc30f25998ef02f9
Luis Mendez <71355340+dluismendezpy@users.noreply.github.com>
2021-12-02 11:22:42 -0400

    Create AskModel.cs

---
+++ AskApp/Models/AskModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AskApp.Models
+{
+    public class AskModel
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+
+        public AskModel()
+        {
+
+        }
+    }
+}
